4a90f2052ab35c68495cf91df42b1a0cb5f43235
i358 <retro98team@gmail.com>
2025-01-02 02:40:07 +0300

    websocket tests

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import Game from './components/Game';
 import './styles/main.scss';
 
+const ws = new WebSocket("ws://localhost:4000/ws/")
+
+ws.onopen = (e) => { 
+  console.log('WebSocket connection established');
+}
+
+ws.onmessage = (m) => {
+  console.log(m.data)
+}
+
+ws.onclose = (r) => {
+  console.log('WebSocket connection closed', r.code, r.reason);
+}
+
+
 function App() {
   return (
     <div className="App">
