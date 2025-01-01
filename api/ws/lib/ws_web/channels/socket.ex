708ccbf4fbf70917f8723ad41daaa7eb9191f617
i358 <retro98team@gmail.com>
2025-01-01 18:03:30 +0300

    .

--- api/ws/lib/ws_web/channels/socket.ex
+++ api/ws/lib/ws_web/channels/socket.ex
@@ -4,10 +4,9 @@ defmodule WsWeb.Socket do
   channel "socket", WsWeb.SocketChannel
 
   def connect(_params, socket, _connect_info) do
-    IO.puts "Biri bağlandı."
+    IO.puts "A client connected."
     {:ok, socket}
   end
 
   def id(_socket), do: nil
 end
- 
