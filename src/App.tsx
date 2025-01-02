5575ec8576acd25ba73dd545f6a60a84f6da3cd1
i358 <retro98team@gmail.com>
2025-01-02 17:01:00 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Game from './components/Game';
 import './styles/main.scss';
+import { WS } from './utils/websocket';
 
 function App() {
+  const [ws_connected, setWSConnected] = useState<boolean>(false);
+WS.on("connect", () => {
+  setWSConnected(true);
+})
+
 return (
     <div className="App">
-      <Game />
+     { ws_connected ?  <Game /> : <>Waiting for WebSocket connection...</> }
     </div>
   );
 }
