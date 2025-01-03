cbad79d24567eb9c50dc23982dc3f8d2a75ff0e5
i358 <retro98team@gmail.com>
2025-01-04 00:42:06 +0300

    wwww

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,9 +1,9 @@
-#![allow(unused)]
-
 mod routes;
 mod structs;
 mod util;
+use response::send_resp;
 use routes::api::v1::*;
+use structs::response::Response;
 use util::*;
 
 use std::{env, net::SocketAddr};
@@ -11,27 +11,30 @@ use std::{env, net::SocketAddr};
 use anyhow::Result;
 
 use axum::{
-  extract::{State},
-  http::{Method, StatusCode},
-  response::Html,
+  extract::State,
+  http::{Method, StatusCode, Uri},
+  response::{Html, Redirect},
   routing::{get, patch, post},
   serve, Json, Router,
 };
 use tower_http::cors::{Any, CorsLayer};
 
 use serde::{Deserialize, Serialize};
-use serde_json::json;
+use serde_json::{json, Value};
 
-use sqlx::{postgres::PgPoolOptions, PgPool};
+use sqlx::{postgres::PgPoolOptions, PgPool, Value};
 
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
+  match dotenvy::dotenv() {
+    Ok(path) => println!("Loaded .env file from {:?}", path),
+    Err(e) => (),
+  }
 
   let port: u16 = env::var("PORT")?.parse::<u16>()?;
-  let addr = SocketAddr::from(([0, 0, 0, 0], port));
+  let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
   let database_url = env::var("DATABASE_URL")?;
 
   let pool = PgPoolOptions::new()
@@ -48,13 +51,23 @@ async fn main() -> Result<()> {
     .allow_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE])
     .allow_headers(Any);
 
+  let redirect_handler = |uri: Uri| async move {
+    let target_url = "https://tictactox.online";
+    Redirect::temporary(target_url)
+  };
+
+  fn test() -> Result<(StatusCode, Json<Value>), (StatusCode, Json<Value>)> {
+    Ok(
+      send_resp(Response { s: StatusCode::OK, t: "ok", m: "ok", e: "ok" })
+    )
+  }
+
   let index = Router::new()
     .with_state(pool)
+    .route("/", get(test))
     .layer(cors);
-
 
   serve(listener, index).await?;
 
   Ok(())
-
 }
